21d5e45425e238c48edad7f64da7e8697eea3c10
SANTA-123 <sebastiansanta0406@gmail.com>
2026-03-18 08:29:04 -0500

    (chore): Definicion de endpoints y url basee de la api para proxima comunicacion con endpoints

---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+
+const URL_BASE = "localhost:8080";
+
+export let end_points = {
+    users: URL_BASE + "/users",
+    merchands: URL_BASE + "/merchands",
+    expenses: URL_BASE + "/expenses",
+    payments_methods: URL_BASE + "/payments_methods",
+    categories: URL_BASE + "/categories",
+}
+
